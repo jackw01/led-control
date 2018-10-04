24113d782151504162eb84412b8de170a6ddd97a
jackw01 <jackw01@users.noreply.github.com>
2018-10-03 23:03:28 -0700

    set up templates

--- ledcontrol/app.py
+++ ledcontrol/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return flask.render_template('index.html')
 
     return app
