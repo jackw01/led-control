f9b2acc1cfa96b2d3e45ab69592be08af5c6b35e
jackw01 <jackw01@users.noreply.github.com>
2018-10-03 21:35:13 -0700

    add argument parser

--- ledcontrol/__init__.py
+++ ledcontrol/__init__.py
@@ -1,16 +1,19 @@
-from flask import Flask
+import argparse
+from ledcontrol.app import create_app
 from werkzeug.serving import run_simple
 
 port = 8080
 
-def create_app():
-    app = Flask(__name__)
-
-    return app
-
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=80,
+                        help='Port to use for web interface. Default: 80')
+    parser.add_argument('--host', default='127.0.0.1',
+                        help='Hostname to use for web interface. Default: 127.0.0.1')
+    args = parser.parse_args()
+
     app = create_app()
-    run_simple('127.0.0.1', int(port), app,
+    run_simple(args.host, int(args.port), app,
                use_reloader=True,
                use_debugger=True,
                use_evalex=True)
