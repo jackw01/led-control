e835ab7d0cf92eb49c9ce8478246625b802c582a
Campbell Vertesi <campbell@vertesi.com>
2021-11-14 15:03:34 +0000

    Add hsv to rgb conversion function in restricted list

--- ledcontrol/animationcontroller.py
+++ ledcontrol/animationcontroller.py
@@ -148,6 +148,7 @@ class AnimationController:
             'fract': utils.fract,
             'blackbody_to_rgb': driver.blackbody_to_rgb,
             'blackbody_correction_rgb': driver.blackbody_correction_rgb,
+            'hsv_to_rgb': self.hsv_to_rgb,
         }
         restricted_locals = {}
         arg_names = ['t', 'dt', 'x', 'y', 'z', 'prev_state']
@@ -276,6 +277,17 @@ class AnimationController:
         self.palette_table = palette_table
         self.palette_length = len(palette['colors'])
         self.update_needed = True
+
+    def hsv_to_rgb(h, s, v):
+        if s == 0.0: v*=255; return (v, v, v)
+        i = int(h*6.) # XXX assume int() truncates!
+        f = (h*6.)-i; p,q,t = int(255*(v*(1.-s))), int(255*(v*(1.-s*f))), int(255*(v*(1.-s*(1.-f)))); v*=255; i%=6
+        if i == 0: return (v, t, p)
+        if i == 1: return (q, v, p)
+        if i == 2: return (p, v, t)
+        if i == 3: return (p, q, v)
+        if i == 4: return (t, p, v)
+        if i == 5: return (v, p, q)
 
     def get_palette_color(self, t):
         'Get color from current palette corresponding to index between 0 and 1'
