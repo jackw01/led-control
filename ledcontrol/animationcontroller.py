103cb71d0daa4c22142d08c39d3f3649d3187741
Campbell Vertesi <campbell@vertesi.com>
2021-11-14 16:18:18 +0100

    Return to stock colorsys function

--- ledcontrol/animationcontroller.py
+++ ledcontrol/animationcontroller.py
@@ -279,9 +279,9 @@ class AnimationController:
         self.update_needed = True
 
     def hsv_to_rgb(h, s, v):
-        if s == 0.0: v*=255; return (v, v, v)
+        if s == 0.0: return (v, v, v)
         i = int(h*6.) # XXX assume int() truncates!
-        f = (h*6.)-i; p,q,t = int(255*(v*(1.-s))), int(255*(v*(1.-s*f))), int(255*(v*(1.-s*(1.-f)))); v*=255; i%=6
+        f = (h*6.)-i; p,q,t = v*(1.-s), v*(1.-s*f), v*(1.-s*(1.-f)); i%=6
         if i == 0: return (v, t, p)
         if i == 1: return (q, v, p)
         if i == 2: return (p, v, t)
